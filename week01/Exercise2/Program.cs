7f2b3ab4837a85bbfd08a4c7b6eaac4b52be1d1c
Ukpono Bassey Essien <basseyp20@outlook.com>
2026-03-04 18:26:58 +0100

    Finished exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter you grade percentage: ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+
+        string letter = "";
+        string sign = "";
+
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = numberGrade % 10;
+
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter == "A" && lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+    
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Keep putting in your best.");
+        }
     }
 }
