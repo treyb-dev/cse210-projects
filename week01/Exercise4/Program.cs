723060f8c8690023845656ab65f317fba5daa15e
Ukpono Bassey Essien <basseyp20@outlook.com>
2026-03-06 17:03:14 +0100

    Finished Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,55 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("\nEnter a list of numbers, type zero when finished.");
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } 
+        while (number != 0);
+
+        int sumTotal = 0;
+        int largestNumber = numbers[0];
+        int smallestPositive = int.MaxValue;
+
+        foreach (int i in numbers)
+        {
+            sumTotal += i;   
+
+            if (i > largestNumber)
+            {
+                largestNumber = i;
+            }
+            if (i > 0 && i < smallestPositive)
+            {
+                smallestPositive = i;
+            }
+        }
+        Console.WriteLine($"\nThe Sum is: {sumTotal}");
+
+        float average = (float)sumTotal / numbers.Count;
+        Console.WriteLine($"The Average is: {average}");
+        
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
